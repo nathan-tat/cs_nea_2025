c675d184512d6968d2557ecbc88cefef861817c5
nathan-tat <nathan.tatkowski@outlook.com>
2024-05-15 14:10:21 +0100

    Implemented GitHub Folder downloader

--- code/installer.py
+++ code/installer.py
@@ -1,15 +1,52 @@
 import ctypes
-import fsspec
+from github import Github, Repository, ContentFile
 import os
-from pathlib import Path
 import sys
+import requests
 import tkinter as tk
 import tkinter.font as tkFont
 from tkinter import filedialog
 
 
+""" 
+The following code downloads a folder from GitHub
+Source: https://github.com/Nordgaren/Github-Folder-Downloader/tree/master
+"""
+
+
+def download(c: ContentFile, out: str):
+    r = requests.get(c.download_url)
+    output_path = f'{out}/{c.path}'
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    with open(output_path, 'wb') as f:
+        print(f'downloading {c.path} to {out}')
+        f.write(r.content)
+
+
+def download_folder(repo: Repository, folder: str, out: str, recursive: bool):
+    contents = repo.get_contents(folder)
+    for c in contents:
+        if c.download_url is None:
+            if recursive:
+                download_folder(repo, c.path, out, recursive)
+            continue
+        download(c, out)
+
+
+def download_file(repo: Repository, folder: str, out: str):
+    c = repo.get_contents(folder)
+    download(c, out)
+
+
+""" 
+^^^ End of code from https://github.com/Nordgaren/Github-Folder-Downloader/tree/master
+"""
+
+
 # the requirements.txt which has all the necessary pip libraries to run the program 
 REQ: str = "https://raw.githubusercontent.com/nathan-tat/cs_nea_2025/main/requirements.txt"
+REPO_NAME: str = "nathan-tat/cs_nea_2025"
+SW_DIR: str = "code/software"
 
 
 def is_admin() -> bool:
@@ -24,9 +61,11 @@ def is_admin() -> bool:
         return False
 
 
-def download_from_github(url: str, destination: str) -> None:
+def download_from_github(destination: str) -> None:
     """ Downloads a given GitHub folder from 'url' to the 'destination' directory """
-    pass
+    g = Github()
+    repo = g.get_repo(REPO_NAME)
+    download_folder(repo, SW_DIR, destination, True)
 
 
 def install_requirements(url: str) -> None:
